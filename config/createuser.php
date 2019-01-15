e52388cfe6d8c6fffd5b54ac007ccd43d154050f
Musa Zhou <musazhou@gmail.com>
2019-01-15 15:15:49 +0800

    update

---
+++ config/createuser.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    * The class name of the user model to be used.
+    */
+    'model' => 'App\Model\User',
+
+    /*
+    * The validation rules to check for user model input.
+    */
+    'validation_rules' => [
+        'name'     => 'string|max:255',
+        'email'    => 'string|email|max:255|unique:users',
+        'password' => 'string|min:6',
+    ],
+
+];
